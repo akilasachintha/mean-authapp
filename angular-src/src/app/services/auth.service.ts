4b7393cd107f91d4336f751bc6ad1c5dacd23e59
Akila Sachintha <akilasachintha@gmail.com>
2021-09-18 00:40:01 +0530

    Edit ports

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -17,6 +17,6 @@ export class AuthService {
   registerUser(user: any){
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'application/json');
-    return this.http.post('http://localhost:3000/users/register', user, {headers: headers});
+    return this.http.post('users/register', user, {headers: headers});
   }
 }
