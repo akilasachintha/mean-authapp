4a773b19a25f878332e5922d124f4347d6f4b649
Akila Sachintha <akilasachintha@gmail.com>
2021-09-18 08:53:17 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,8 @@
 module.exports = {
+<<<<<<< HEAD
     database: 'mongodb+srv://********:********@akilamongodb.dbkf5.mongodb.net/meanAuthAppDB?retryWrites=true&w=majority',
+=======
+    database: 'mongodb://localhost:27017/meanauthdb',
+>>>>>>> parent of 5526951 (Update database.js)
     secret: 'mysecret'
 }
